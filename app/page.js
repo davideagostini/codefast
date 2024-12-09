7688b804f7ef23301150a1a0cb3818d87f85149f
Davide Agostini <323626+davideagostini@users.noreply.github.com>
2024-12-09 12:50:50 +0100

    add hero and header section

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import ButtonLogin from "@/component/ButtonLogin";
+import ButtonLogin from "@/components/ButtonLogin";
 
 export default function Home() {
   const isLoggedIn = true;
@@ -9,7 +9,7 @@ export default function Home() {
       <section className="bg-base-200">
         <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
           <div className="font-bold">CodeFastSaaS</div>
-          <div className="space-x-4">
+          <div className="space-x-4 max-md:hidden">
             <a className="link link-hover">Pricing</a>
             <a className="link link-hover">FAQ</a>
           </div>
@@ -20,14 +20,14 @@ export default function Home() {
       </section>
 
       <section className="text-center py-32 px-8 max-w-3xl mx-auto">
-        <h1 className="text-4xl font-extrabold mb-6">
+        <h1 className="lg:text-5xl text-4xl font-extrabold mb-6">
           Collect customer feedback to build better products
         </h1>
         <div className="opacity-90 mb-10">
           Create a feedback board in minutes, prioritize features, and build
           products your customer will love.
         </div>
-        <ButtonLogin />
+        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
       </section>
     </main>
   );
