7a0a48b003ab1808407fae75f83e3f77b73fab56
Davide Agostini <323626+davideagostini@users.noreply.github.com>
2024-12-13 14:33:54 +0100

    polish homepage

--- app/page.js
+++ app/page.js
@@ -1,17 +1,31 @@
 import ButtonLogin from "@/components/ButtonLogin";
+import ListItem from "@/components/ListItem";
+import FAQListItem from "@/components/FAQListItem";
 
 export default function Home() {
   const isLoggedIn = true;
   const name = "Davide";
 
+  const pricingFeatureList = [
+    "Collect customer feedback",
+    "Unlimited boards",
+    "Admin dashboard",
+    "24/7 support",
+  ];
+
   return (
     <main>
+      {/* HEADER */}
       <section className="bg-base-200">
         <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
           <div className="font-bold">CodeFastSaaS</div>
           <div className="space-x-4 max-md:hidden">
-            <a className="link link-hover">Pricing</a>
-            <a className="link link-hover">FAQ</a>
+            <a className="link link-hover" href="#pricing">
+              Pricing
+            </a>
+            <a className="link link-hover" href="#faq">
+              FAQ
+            </a>
           </div>
           <div>
             <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
@@ -19,6 +33,7 @@ export default function Home() {
         </div>
       </section>
 
+      {/* HERO */}
       <section className="text-center py-32 px-8 max-w-3xl mx-auto">
         <h1 className="lg:text-5xl text-4xl font-extrabold mb-6">
           Collect customer feedback to build better products
@@ -28,6 +43,61 @@ export default function Home() {
           products your customer will love.
         </div>
         <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+
+      {/* PRICING */}
+      <section id="pricing" className="bg-base-200">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">
+            Pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            A pricing that adapts to your needs
+          </h2>
+
+          <div className="p-8 bg-base-100 max-w-96 rounded-3xl mx-auto space-y-6">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">$19</div>
+              <div className="text-sm uppercase font-medium opacity-60">
+                /month
+              </div>
+            </div>
+
+            <ul className="space-y-2">
+              {pricingFeatureList.map((el, index) => {
+                return <ListItem text={el} key={index} />;
+              })}
+            </ul>
+
+            <ButtonLogin
+              isLoggedIn={isLoggedIn}
+              name={name}
+              extraStyle="w-full"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* FAQ */}
+      <section id="faq" className="bg-base-100">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">
+            FAQ
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            Frequently Asked Questions
+          </h2>
+
+          <ul className="max-w-lg mx-auto">
+            {[
+              { question: "What do I get exactly?", answer: "Lorem Ipsum" },
+              { question: "Can I get a refund?", answer: "Lorem Ipsum" },
+              { question: "I have another question", answer: "Lorem Ipsum" },
+            ].map((qa) => (
+              <FAQListItem key={qa.question} qa={qa} />
+            ))}
+          </ul>
+        </div>
       </section>
     </main>
   );
